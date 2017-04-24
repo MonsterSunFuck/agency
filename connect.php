1f9778bb2b8c2fd45b959b781de160d3f1503caf
Sergei Terehov <isergeiterehov@gmail.com>
2017-04-24 13:49:46 +0300

    Подключение к базе вынесено в отдельный файл. Добавлен phpinfo

--- connect.php
+++ connect.php
@@ -1,12 +1,8 @@
 <?php
-	$dbhost="localhost";
-	$dbname="agency";
-	$dbuser="root";
-	$dbpass="123";
+	require('connect-local.php');
+
 	$dbconn=mysql_connect($dbhost,$dbuser,$dbpass);
 	if(!$dbconn) die ("Нет подключения ");
 
 	$dbselect=mysql_select_db($dbname);
 	if(!$dbname)die("Error");
-
-?>
